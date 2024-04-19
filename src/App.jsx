90a23376ff85c78f03d75fea3a8f455846ad1507
iradiane <iradudiane0@gmail.com>
2024-04-19 08:54:52 +0200

    react portfolio

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import SocialLinks from "./components/sociallinks";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
+const App = () => {
+  return (
+    <div>
+<NavBar/>
+<Home/>
+<About/>
+<Portfolio/>
+<Experience/>
+<Contact/>
+<SocialLinks/>
+    </div>
+  );
+}
+
+export default App
